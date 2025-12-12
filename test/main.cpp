6ed5a1edcaa51d6e606d590932a16600d68de1ab
Helheimiss <171362259+Helheimiss@users.noreply.github.com>
2025-12-13 00:13:40 +0300

    changes test

--- test/main.cpp
+++ test/main.cpp
@@ -48,15 +48,62 @@ void test1()
 }
 
 
-void test2()
+void test2(const std::string& CharCodeTarget, const std::string& ValueTarget)
 {
+    tinyxml2::XMLDocument doc;
+    tinyxml2::XMLError res = doc.Parse(DATA.c_str());
+    assert(res == tinyxml2::XML_SUCCESS);
 
+    tinyxml2::XMLElement* root = doc.RootElement();
+    assert(root != nullptr);
+
+    tinyxml2::XMLElement* valute = root->FirstChildElement("Valute");
+    assert(valute != nullptr);
+
+    std::string RES_CC {};
+    std::string RES_VE {};
+
+    while (valute != nullptr) {
+        const char* id = valute->Attribute("ID");
+        std::cout << "ID: " << (id ? id : "нет") << std::endl;
+
+        tinyxml2::XMLElement* CC = valute->FirstChildElement("CharCode");
+        tinyxml2::XMLElement* VE = valute->FirstChildElement("Value");
+
+        if (CC && VE && CC->GetText() && VE->GetText())
+        {
+            std::cout << "CharCode: " << CC->GetText() << std::endl;
+            std::cout << "Value: " << VE->GetText() << std::endl;
+
+            RES_CC = CC->GetText();
+            RES_VE = VE->GetText();
+
+            break;
+        }
+
+
+        valute = valute->NextSiblingElement("Valute");
+    }
+
+
+
+    assert(CharCodeTarget == RES_CC);
+    assert(ValueTarget == RES_VE);
 }
 
 
 int main(int argc, char *argv[])
 {
+    std::cout << "press enter to continue..." << std::endl;
+    getchar();
+
+    std::cout << "test1 all data" << std::endl;
     test1();
+    getchar();
+
+    std::cout << "test2 find by CharCode" << std::endl;
+    test2("AUD", "53,1079");
+    getchar();
 
 
     puts("done");
