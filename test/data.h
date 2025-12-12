cccefbc55b53cd35c4e169d681bae88ed6a5fb75
Helheimiss <171362259+Helheimiss@users.noreply.github.com>
2025-12-12 23:52:09 +0300

    added data test

---
+++ test/data.h
@@ -0,0 +1,446 @@
+#pragma once
+#include <string>
+
+inline const std::string DATA = R"(
+<ValCurs Date="13.12.2025" name="Foreign Currency Market">
+<Valute ID="R01010">
+<NumCode>036</NumCode>
+<CharCode>AUD</CharCode>
+<Nominal>1</Nominal>
+<Name>Australian Dollar</Name>
+<Value>53,1079</Value>
+<VunitRate>53,1079</VunitRate>
+</Valute>
+<Valute ID="R01020A">
+<NumCode>944</NumCode>
+<CharCode>AZN</CharCode>
+<Nominal>1</Nominal>
+<Name>Azerbaijan Manat</Name>
+<Value>46,8998</Value>
+<VunitRate>46,8998</VunitRate>
+</Valute>
+<Valute ID="R01030">
+<NumCode>012</NumCode>
+<CharCode>DZD</CharCode>
+<Nominal>100</Nominal>
+<Name>Algerian Dinar</Name>
+<Value>61,5323</Value>
+<VunitRate>0,615323</VunitRate>
+</Valute>
+<Valute ID="R01035">
+<NumCode>826</NumCode>
+<CharCode>GBP</CharCode>
+<Nominal>1</Nominal>
+<Name>Pound Sterling</Name>
+<Value>107,1167</Value>
+<VunitRate>107,1167</VunitRate>
+</Valute>
+<Valute ID="R01060">
+<NumCode>051</NumCode>
+<CharCode>AMD</CharCode>
+<Nominal>100</Nominal>
+<Name>Armenian Dram</Name>
+<Value>20,8842</Value>
+<VunitRate>0,208842</VunitRate>
+</Valute>
+<Valute ID="R01080">
+<NumCode>048</NumCode>
+<CharCode>BHD</CharCode>
+<Nominal>1</Nominal>
+<Name>Bahraini Dinar</Name>
+<Value>212,0010</Value>
+<VunitRate>212,001</VunitRate>
+</Valute>
+<Valute ID="R01090B">
+<NumCode>933</NumCode>
+<CharCode>BYN</CharCode>
+<Nominal>1</Nominal>
+<Name>Belarusian Ruble</Name>
+<Value>27,0426</Value>
+<VunitRate>27,0426</VunitRate>
+</Valute>
+<Valute ID="R01100">
+<NumCode>975</NumCode>
+<CharCode>BGN</CharCode>
+<Nominal>1</Nominal>
+<Name>Bulgarian Lev</Name>
+<Value>47,7523</Value>
+<VunitRate>47,7523</VunitRate>
+</Valute>
+<Valute ID="R01105">
+<NumCode>068</NumCode>
+<CharCode>BOB</CharCode>
+<Nominal>1</Nominal>
+<Name>Bolivian Boliviano</Name>
+<Value>11,5383</Value>
+<VunitRate>11,5383</VunitRate>
+</Valute>
+<Valute ID="R01115">
+<NumCode>986</NumCode>
+<CharCode>BRL</CharCode>
+<Nominal>1</Nominal>
+<Name>Brazilian Real</Name>
+<Value>14,7010</Value>
+<VunitRate>14,701</VunitRate>
+</Valute>
+<Valute ID="R01135">
+<NumCode>348</NumCode>
+<CharCode>HUF</CharCode>
+<Nominal>100</Nominal>
+<Name>Forint</Name>
+<Value>24,3687</Value>
+<VunitRate>0,243687</VunitRate>
+</Valute>
+<Valute ID="R01150">
+<NumCode>704</NumCode>
+<CharCode>VND</CharCode>
+<Nominal>10000</Nominal>
+<Name>Dong</Name>
+<Value>31,7042</Value>
+<VunitRate>0,00317042</VunitRate>
+</Valute>
+<Valute ID="R01200">
+<NumCode>344</NumCode>
+<CharCode>HKD</CharCode>
+<Nominal>1</Nominal>
+<Name>Hong Kong Dollar</Name>
+<Value>10,2586</Value>
+<VunitRate>10,2586</VunitRate>
+</Valute>
+<Valute ID="R01210">
+<NumCode>981</NumCode>
+<CharCode>GEL</CharCode>
+<Nominal>1</Nominal>
+<Name>Lari</Name>
+<Value>29,5393</Value>
+<VunitRate>29,5393</VunitRate>
+</Valute>
+<Valute ID="R01215">
+<NumCode>208</NumCode>
+<CharCode>DKK</CharCode>
+<Nominal>1</Nominal>
+<Name>Danish Krone</Name>
+<Value>12,5043</Value>
+<VunitRate>12,5043</VunitRate>
+</Valute>
+<Valute ID="R01230">
+<NumCode>784</NumCode>
+<CharCode>AED</CharCode>
+<Nominal>1</Nominal>
+<Name>UAE Dirham</Name>
+<Value>21,7099</Value>
+<VunitRate>21,7099</VunitRate>
+</Valute>
+<Valute ID="R01235">
+<NumCode>840</NumCode>
+<CharCode>USD</CharCode>
+<Nominal>1</Nominal>
+<Name>US Dollar</Name>
+<Value>79,7296</Value>
+<VunitRate>79,7296</VunitRate>
+</Valute>
+<Valute ID="R01239">
+<NumCode>978</NumCode>
+<CharCode>EUR</CharCode>
+<Nominal>1</Nominal>
+<Name>Euro</Name>
+<Value>93,5626</Value>
+<VunitRate>93,5626</VunitRate>
+</Valute>
+<Valute ID="R01240">
+<NumCode>818</NumCode>
+<CharCode>EGP</CharCode>
+<Nominal>10</Nominal>
+<Name>Egyptian Pound</Name>
+<Value>16,7732</Value>
+<VunitRate>1,67732</VunitRate>
+</Valute>
+<Valute ID="R01270">
+<NumCode>356</NumCode>
+<CharCode>INR</CharCode>
+<Nominal>100</Nominal>
+<Name>Indian Rupee</Name>
+<Value>88,2079</Value>
+<VunitRate>0,882079</VunitRate>
+</Valute>
+<Valute ID="R01280">
+<NumCode>360</NumCode>
+<CharCode>IDR</CharCode>
+<Nominal>10000</Nominal>
+<Name>Rupiah</Name>
+<Value>47,8339</Value>
+<VunitRate>0,00478339</VunitRate>
+</Valute>
+<Valute ID="R01300">
+<NumCode>364</NumCode>
+<CharCode>IRR</CharCode>
+<Nominal>100000</Nominal>
+<Name>Iranian Rial</Name>
+<Value>12,1149</Value>
+<VunitRate>0,000121149</VunitRate>
+</Valute>
+<Valute ID="R01335">
+<NumCode>398</NumCode>
+<CharCode>KZT</CharCode>
+<Nominal>100</Nominal>
+<Name>Tenge</Name>
+<Value>15,3320</Value>
+<VunitRate>0,15332</VunitRate>
+</Valute>
+<Valute ID="R01350">
+<NumCode>124</NumCode>
+<CharCode>CAD</CharCode>
+<Nominal>1</Nominal>
+<Name>Canadian Dollar</Name>
+<Value>57,8841</Value>
+<VunitRate>57,8841</VunitRate>
+</Valute>
+<Valute ID="R01355">
+<NumCode>634</NumCode>
+<CharCode>QAR</CharCode>
+<Nominal>1</Nominal>
+<Name>Qatari Rial</Name>
+<Value>21,9037</Value>
+<VunitRate>21,9037</VunitRate>
+</Valute>
+<Valute ID="R01370">
+<NumCode>417</NumCode>
+<CharCode>KGS</CharCode>
+<Nominal>100</Nominal>
+<Name>Som</Name>
+<Value>91,1716</Value>
+<VunitRate>0,911716</VunitRate>
+</Valute>
+<Valute ID="R01375">
+<NumCode>156</NumCode>
+<CharCode>CNY</CharCode>
+<Nominal>1</Nominal>
+<Name>Yuan Renminbi</Name>
+<Value>11,2726</Value>
+<VunitRate>11,2726</VunitRate>
+</Valute>
+<Valute ID="R01395">
+<NumCode>192</NumCode>
+<CharCode>CUP</CharCode>
+<Nominal>10</Nominal>
+<Name>Cuban peso</Name>
+<Value>33,2207</Value>
+<VunitRate>3,32207</VunitRate>
+</Valute>
+<Valute ID="R01500">
+<NumCode>498</NumCode>
+<CharCode>MDL</CharCode>
+<Nominal>10</Nominal>
+<Name>Moldovan Leu</Name>
+<Value>47,0570</Value>
+<VunitRate>4,7057</VunitRate>
+</Valute>
+<Valute ID="R01503">
+<NumCode>496</NumCode>
+<CharCode>MNT</CharCode>
+<Nominal>1000</Nominal>
+<Name>Mongolia Tugrik</Name>
+<Value>22,4919</Value>
+<VunitRate>0,0224919</VunitRate>
+</Valute>
+<Valute ID="R01520">
+<NumCode>566</NumCode>
+<CharCode>NGN</CharCode>
+<Nominal>1000</Nominal>
+<Name>Nigeria Naira</Name>
+<Value>54,7569</Value>
+<VunitRate>0,0547569</VunitRate>
+</Valute>
+<Valute ID="R01530">
+<NumCode>554</NumCode>
+<CharCode>NZD</CharCode>
+<Nominal>1</Nominal>
+<Name>New Zealand Dollar</Name>
+<Value>46,3707</Value>
+<VunitRate>46,3707</VunitRate>
+</Valute>
+<Valute ID="R01535">
+<NumCode>578</NumCode>
+<CharCode>NOK</CharCode>
+<Nominal>10</Nominal>
+<Name>Norwegian Krone</Name>
+<Value>78,9809</Value>
+<VunitRate>7,89809</VunitRate>
+</Valute>
+<Valute ID="R01540">
+<NumCode>512</NumCode>
+<CharCode>OMR</CharCode>
+<Nominal>1</Nominal>
+<Name>Omani Rial</Name>
+<Value>206,3454</Value>
+<VunitRate>206,3454</VunitRate>
+</Valute>
+<Valute ID="R01565">
+<NumCode>985</NumCode>
+<CharCode>PLN</CharCode>
+<Nominal>1</Nominal>
+<Name>Zloty</Name>
+<Value>22,1305</Value>
+<VunitRate>22,1305</VunitRate>
+</Valute>
+<Valute ID="R01580">
+<NumCode>682</NumCode>
+<CharCode>SAR</CharCode>
+<Nominal>1</Nominal>
+<Name>Saudi Riyal</Name>
+<Value>21,2612</Value>
+<VunitRate>21,2612</VunitRate>
+</Valute>
+<Valute ID="R01585F">
+<NumCode>946</NumCode>
+<CharCode>RON</CharCode>
+<Nominal>1</Nominal>
+<Name>Romanian Leu</Name>
+<Value>18,3683</Value>
+<VunitRate>18,3683</VunitRate>
+</Valute>
+<Valute ID="R01589">
+<NumCode>960</NumCode>
+<CharCode>XDR</CharCode>
+<Nominal>1</Nominal>
+<Name>SDR (Special Drawing Right)</Name>
+<Value>108,9401</Value>
+<VunitRate>108,9401</VunitRate>
+</Valute>
+<Valute ID="R01625">
+<NumCode>702</NumCode>
+<CharCode>SGD</CharCode>
+<Nominal>1</Nominal>
+<Name>Singapore Dollar</Name>
+<Value>61,7198</Value>
+<VunitRate>61,7198</VunitRate>
+</Valute>
+<Valute ID="R01670">
+<NumCode>972</NumCode>
+<CharCode>TJS</CharCode>
+<Nominal>10</Nominal>
+<Name>Somoni</Name>
+<Value>86,3136</Value>
+<VunitRate>8,63136</VunitRate>
+</Valute>
+<Valute ID="R01675">
+<NumCode>764</NumCode>
+<CharCode>THB</CharCode>
+<Nominal>10</Nominal>
+<Name>Baht</Name>
+<Value>25,1878</Value>
+<VunitRate>2,51878</VunitRate>
+</Valute>
+<Valute ID="R01685">
+<NumCode>050</NumCode>
+<CharCode>BDT</CharCode>
+<Nominal>100</Nominal>
+<Name>Bangladesh Taka</Name>
+<Value>65,1481</Value>
+<VunitRate>0,651481</VunitRate>
+</Valute>
+<Valute ID="R01700J">
+<NumCode>949</NumCode>
+<CharCode>TRY</CharCode>
+<Nominal>10</Nominal>
+<Name>Turkish Lira</Name>
+<Value>18,7279</Value>
+<VunitRate>1,87279</VunitRate>
+</Valute>
+<Valute ID="R01710A">
+<NumCode>934</NumCode>
+<CharCode>TMT</CharCode>
+<Nominal>1</Nominal>
+<Name>Turkmenistan New Manat</Name>
+<Value>22,7799</Value>
+<VunitRate>22,7799</VunitRate>
+</Valute>
+<Valute ID="R01717">
+<NumCode>860</NumCode>
+<CharCode>UZS</CharCode>
+<Nominal>10000</Nominal>
+<Name>Uzbekistan Sum</Name>
+<Value>66,1796</Value>
+<VunitRate>0,00661796</VunitRate>
+</Valute>
+<Valute ID="R01720">
+<NumCode>980</NumCode>
+<CharCode>UAH</CharCode>
+<Nominal>10</Nominal>
+<Name>Hryvnia</Name>
+<Value>18,8610</Value>
+<VunitRate>1,8861</VunitRate>
+</Valute>
+<Valute ID="R01760">
+<NumCode>203</NumCode>
+<CharCode>CZK</CharCode>
+<Nominal>10</Nominal>
+<Name>Czech Koruna</Name>
+<Value>38,5652</Value>
+<VunitRate>3,85652</VunitRate>
+</Valute>
+<Valute ID="R01770">
+<NumCode>752</NumCode>
+<CharCode>SEK</CharCode>
+<Nominal>10</Nominal>
+<Name>Swedish Krona</Name>
+<Value>86,1183</Value>
+<VunitRate>8,61183</VunitRate>
+</Valute>
+<Valute ID="R01775">
+<NumCode>756</NumCode>
+<CharCode>CHF</CharCode>
+<Nominal>1</Nominal>
+<Name>Swiss Franc</Name>
+<Value>100,3140</Value>
+<VunitRate>100,314</VunitRate>
+</Valute>
+<Valute ID="R01800">
+<NumCode>230</NumCode>
+<CharCode>ETB</CharCode>
+<Nominal>100</Nominal>
+<Name>Ethiopian Birr</Name>
+<Value>51,3916</Value>
+<VunitRate>0,513916</VunitRate>
+</Valute>
+<Valute ID="R01805F">
+<NumCode>941</NumCode>
+<CharCode>RSD</CharCode>
+<Nominal>100</Nominal>
+<Name>Serbian Dinar</Name>
+<Value>79,7527</Value>
+<VunitRate>0,797527</VunitRate>
+</Valute>
+<Valute ID="R01810">
+<NumCode>710</NumCode>
+<CharCode>ZAR</CharCode>
+<Nominal>10</Nominal>
+<Name>Rand</Name>
+<Value>47,3544</Value>
+<VunitRate>4,73544</VunitRate>
+</Valute>
+<Valute ID="R01815">
+<NumCode>410</NumCode>
+<CharCode>KRW</CharCode>
+<Nominal>1000</Nominal>
+<Name>Won</Name>
+<Value>54,2563</Value>
+<VunitRate>0,0542563</VunitRate>
+</Valute>
+<Valute ID="R01820">
+<NumCode>392</NumCode>
+<CharCode>JPY</CharCode>
+<Nominal>100</Nominal>
+<Name>Yen</Name>
+<Value>51,2138</Value>
+<VunitRate>0,512138</VunitRate>
+</Valute>
+<Valute ID="R02005">
+<NumCode>104</NumCode>
+<CharCode>MMK</CharCode>
+<Nominal>1000</Nominal>
+<Name>Kyat</Name>
+<Value>37,9665</Value>
+<VunitRate>0,0379665</VunitRate>
+</Valute>
+</ValCurs>)";
--- test/main.cpp
+++ test/main.cpp
@@ -1,41 +1,15 @@
 #include "tinyxml2.h"
 
+#include "data.h"
+
 #include <iostream>
 #include <string>
 
 
 double test1()
 {
-    std::string xml_data =
-R"(<ValCurs Date="13.12.2025" name="Foreign Currency Market">
-    <Valute ID="R01010">
-        <NumCode>036</NumCode>
-        <CharCode>AUD</CharCode>
-        <Nominal>1</Nominal>
-        <Name>Australian Dollar</Name>
-        <Value>53,1079</Value>
-        <VunitRate>53,1079</VunitRate>
-    </Valute>
-        <Valute ID="R01020A">
-        <NumCode>944</NumCode>
-        <CharCode>AZN</CharCode>
-        <Nominal>1</Nominal>
-        <Name>Azerbaijan Manat</Name>
-        <Value>46,8998</Value>
-        <VunitRate>46,8998</VunitRate>
-    </Valute>
-        <Valute ID="R01030">
-        <NumCode>012</NumCode>
-        <CharCode>DZD</CharCode>
-        <Nominal>100</Nominal>
-        <Name>Algerian Dinar</Name>
-        <Value>61,5323</Value>
-        <VunitRate>0,615323</VunitRate>
-    </Valute>
-</ValCurs>)";
-
     tinyxml2::XMLDocument doc;
-    tinyxml2::XMLError res = doc.Parse(xml_data.c_str());
+    tinyxml2::XMLError res = doc.Parse(DATA.c_str());
     assert(res == tinyxml2::XML_SUCCESS);
 
     tinyxml2::XMLElement* root = doc.RootElement();
